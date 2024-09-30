15412b7d1f5230f776ed77967c685f8cf64c1f3d
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-30 23:59:30 +0900

    init : project

---
+++ src/main/java/com/f4/fqs/FqsApplication.java
@@ -0,0 +1,13 @@
+package com.f4.fqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f4/fqs/FqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f4.fqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
