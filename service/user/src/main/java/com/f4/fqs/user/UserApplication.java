a4696c4662462c87843341d8b2c9b42d955b473d
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-01 03:21:51 +0900

    add : user server

---
+++ service/user/src/main/java/com/f4/fqs/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.f4.fqs.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserApplication.class, args);
+  }
+
+}
