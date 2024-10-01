3c0b6de0bacd69ef64789372bf57b456f53a14ef
dragon0622 <iamdragon.dev@gmail.com>
2024-10-01 23:18:12 +0900

    feat: user entity 선언

---
+++ service/user/src/main/java/com/f4/fqs/user/model/User.java
@@ -0,0 +1,37 @@
+package com.f4.fqs.user.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+}
