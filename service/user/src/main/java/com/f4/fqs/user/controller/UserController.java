b6815a842f055932189800175fdd45601ae821df
dragon0622 <iamdragon.dev@gmail.com>
2024-10-02 16:16:15 +0900

    feat: 로그인 구현

--- service/user/src/main/java/com/f4/fqs/user/controller/UserController.java
+++ service/user/src/main/java/com/f4/fqs/user/controller/UserController.java
@@ -3,8 +3,8 @@ package com.f4.fqs.user.controller;
 
 import com.f4.fqs.user.client.AuthServiceClient;
 import com.f4.fqs.user.model.User;
-import com.f4.fqs.user.dto.SignInRequestDto;
-import com.f4.fqs.user.dto.SignupResponseDto;
+import com.f4.fqs.user.dto.SignUpRequestDto;
+import com.f4.fqs.user.dto.SignUpResponseDto;
 import com.f4.fqs.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -12,23 +12,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
     private final AuthServiceClient authServiceClient;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid SignInRequestDto requestDto, HttpServletResponse response){
+    public ResponseEntity<?> signup(@RequestBody @Valid SignUpRequestDto requestDto, HttpServletResponse response){
         User user = userService.signup(requestDto);
+
+        return ResponseEntity.ok(SignUpResponseDto.toResponse(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid SignUpRequestDto requestDto, HttpServletResponse response){
+        User user = userService.login(requestDto);
 
         String jwt = authServiceClient.createAccessToken(user.getEmail());
 
         response.addHeader("Authorization", "Bearer " + jwt);
 
-        return ResponseEntity.ok(SignupResponseDto.toResponse(user));
+        return ResponseEntity.ok(SignUpResponseDto.toResponse(user));
     }
 }
--- service/user/src/main/java/com/f4/fqs/user/service/UserService.java
+++ service/user/src/main/java/com/f4/fqs/user/service/UserService.java
@@ -1,7 +1,7 @@
 package com.f4.fqs.user.service;
 
 import com.f4.fqs.user.model.User;
-import com.f4.fqs.user.dto.SignInRequestDto;
+import com.f4.fqs.user.dto.SignUpRequestDto;
 import com.f4.fqs.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +16,7 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Transactional
-    public User signup(SignInRequestDto requestDto) {
+    public User signup(SignUpRequestDto requestDto) {
 
         if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
@@ -26,5 +26,17 @@ public class UserService {
 
         User user = new User(requestDto.getEmail(), encodedPassword, requestDto.getRole());
         return userRepository.save(user);
+    }
+
+    public User login(SignUpRequestDto requestDto) {
+
+        User user = userRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("등록된 이메일이 아닙니다."));
+
+        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 틀립니다.");
+        }
+
+        return user;
     }
 }
