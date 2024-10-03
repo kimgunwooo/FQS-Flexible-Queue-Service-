52922499a686e5120be8b3302df6cdffa2defde8
dragon0622 <iamdragon.dev@gmail.com>
2024-10-04 01:20:38 +0900

    feat: 테이블명 변경

--- service/user/src/main/java/com/f4/fqs/user/model/User.java
+++ service/user/src/main/java/com/f4/fqs/user/model/User.java
@@ -6,11 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
+@Table(name = "p_user")
 @NoArgsConstructor
 public class User {
 
