e9c6320232f421398e8fe0de632aa172d3bb46e2
dragon0622 <iamdragon.dev@gmail.com>
2024-10-04 01:22:27 +0900

    feat: 코드 정리

--- service/auth/src/main/java/com/f4/fqs/auth/AuthService.java
+++ service/auth/src/main/java/com/f4/fqs/auth/AuthService.java
@@ -1,6 +1,7 @@
 package com.f4.fqs.auth;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,7 +32,7 @@ public class AuthService {
                 .issuer(issuer)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + accessExpiration))
-                .signWith(secretKey, io.jsonwebtoken.SignatureAlgorithm.HS512)
+                .signWith(secretKey, SignatureAlgorithm.HS512)
                 .compact();
     }
 }
