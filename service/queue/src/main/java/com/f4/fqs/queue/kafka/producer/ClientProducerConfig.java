d51eae2dbc7b5760f2859f13aa650e207552f63a
Minsu Jo <tok0419@naver.com>
2024-10-04 14:04:35 +0900

    Feat : kafka default setting on application layer

---
+++ service/queue/src/main/java/com/f4/fqs/queue/kafka/producer/ClientProducerConfig.java
@@ -0,0 +1,30 @@
+package com.f4.fqs.queue.kafka.producer;
+
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.Map;
+
+@Configuration
+public class ClientProducerConfig {
+
+    @Bean("default-client-producer")
+    public KafkaTemplate<String, String> kafkaTemplate() {
+
+        return new KafkaTemplate<>(
+                new DefaultKafkaProducerFactory<>(
+                        Map.of(
+                            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                            ProducerConfig.ACKS_CONFIG, "1"
+                        )
+                )
+        );
+    }
+
+}
