5287aeec9ff4d9b6b62d7acd340f53d0ba6d6783
dragon0622 <iamdragon.dev@gmail.com>
2024-10-04 14:16:50 +0900

    feat: feign annotation 추가

--- service/auth/src/main/java/com/f4/fqs/auth/AuthApplication.java
+++ service/auth/src/main/java/com/f4/fqs/auth/AuthApplication.java
@@ -2,7 +2,9 @@ package com.f4.fqs.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AuthApplication {
 
