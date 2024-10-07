d9bbcce72f8a5b71010bf08c2c10cce511f167f0
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-07 12:45:35 +0900

    chore : queue server mysql schema 수정

--- service/queue/src/main/resources/schema.sql
+++ service/queue/src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 -- 테이블이 존재할 경우 삭제
 DROP TABLE IF EXISTS queue;
+DROP TABLE IF EXISTS queue_package;
+DROP TABLE IF EXISTS queue_beans;
 
 -- 테이블 생성
 CREATE TABLE queue
@@ -11,7 +13,28 @@ CREATE TABLE queue
     expiration_time             INT          NOT NULL,
     message_order_guaranteed    BOOLEAN      NOT NULL,
     message_duplication_allowed BOOLEAN      NOT NULL,
-    secret_key                  VARCHAR(255) NOT NULL,
+    secret_key                  VARCHAR(255) NOT NULL UNIQUE,
+    created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE queue_package
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    producer_id BIGINT NOT NULL,
+    consumer_id BIGINT NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (producer_id) REFERENCES queue_beans(id),
+    FOREIGN KEY (consumer_id) REFERENCES queue_beans(id)
+);
+
+CREATE TABLE queue_beans
+(
+    id                          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    bean_name                   VARCHAR(16) NOT NULL,
+    message_order_guaranteed    BOOLEAN     NOT NULL,
+    message_duplication_allowed BOOLEAN     NOT NULL,
     created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
