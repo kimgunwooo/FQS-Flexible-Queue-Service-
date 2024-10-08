47e181cee4432a762c19e85e8ef93e167ce05397
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-08 10:58:20 +0900

    chore : queue server schema update

--- service/queue/src/main/resources/schema.sql
+++ service/queue/src/main/resources/schema.sql
@@ -1,19 +1,16 @@
 -- 테이블이 존재할 경우 삭제
 DROP TABLE IF EXISTS queue;
 DROP TABLE IF EXISTS queue_package;
-DROP TABLE IF EXISTS queue_beans;
+DROP TABLE IF EXISTS queue_bean;
+
 
 -- 테이블 생성
-CREATE TABLE queue
+CREATE TABLE queue_bean
 (
     id                          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name                        VARCHAR(16)  NOT NULL,
-    message_retention_period    INT          NOT NULL,
-    max_message_size            INT          NOT NULL,
-    expiration_time             INT          NOT NULL,
-    message_order_guaranteed    BOOLEAN      NOT NULL,
-    message_duplication_allowed BOOLEAN      NOT NULL,
-    secret_key                  VARCHAR(255) NOT NULL UNIQUE,
+    bean_name                   VARCHAR(16) NOT NULL,
+    message_order_guaranteed    BOOLEAN     NOT NULL,
+    message_duplication_allowed BOOLEAN     NOT NULL,
     created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -25,16 +22,23 @@ CREATE TABLE queue_package
     consumer_id BIGINT NOT NULL,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (producer_id) REFERENCES queue_beans(id),
-    FOREIGN KEY (consumer_id) REFERENCES queue_beans(id)
+    FOREIGN KEY (producer_id) REFERENCES queue_bean (id) ON DELETE CASCADE,
+    FOREIGN KEY (consumer_id) REFERENCES queue_bean (id) ON DELETE CASCADE
 );
 
-CREATE TABLE queue_beans
+CREATE TABLE queue
 (
     id                          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    bean_name                   VARCHAR(16) NOT NULL,
-    message_order_guaranteed    BOOLEAN     NOT NULL,
-    message_duplication_allowed BOOLEAN     NOT NULL,
+    user_id                     BIGINT       NOT NULL,
+    queue_package_id            BIGINT       NOT NULL,
+    name                        VARCHAR(16)  NOT NULL UNIQUE,
+    message_retention_period    INT          NOT NULL,
+    max_message_size            INT          NOT NULL,
+    expiration_time             INT          NOT NULL,
+    message_order_guaranteed    BOOLEAN      NOT NULL,
+    message_duplication_allowed BOOLEAN      NOT NULL,
+    secret_key                  VARCHAR(255) NOT NULL UNIQUE,
     created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (queue_package_id) REFERENCES queue_package (id) ON DELETE CASCADE
 );
