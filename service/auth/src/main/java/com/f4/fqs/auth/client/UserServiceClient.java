7c3c9e83729a70efec285c4e97ea13d526bdd930
dragon0622 <iamdragon.dev@gmail.com>
2024-10-08 10:02:44 +0900

    feat: user login 분리

--- service/auth/src/main/java/com/f4/fqs/auth/client/UserServiceClient.java
+++ service/auth/src/main/java/com/f4/fqs/auth/client/UserServiceClient.java
@@ -1,5 +1,6 @@
 package com.f4.fqs.auth.client;
 
+import com.f4.fqs.auth.dto.LogInIAMRequestDto;
 import com.f4.fqs.auth.dto.LogInRequestDto;
 import com.f4.fqs.auth.dto.SignUpRequestDto;
 import com.f4.fqs.auth.dto.UserDto;
@@ -14,7 +15,10 @@ public interface UserServiceClient {
     @PostMapping("/user/signup")
     ResponseEntity<UserDto> signup(@RequestBody SignUpRequestDto requestDto);
 
-    @PostMapping("/user/login")
+    @PostMapping("/user/login/root")
     ResponseEntity<UserDto> login(@RequestBody LogInRequestDto requestDto);
+
+    @PostMapping("/user/login/iam")
+    ResponseEntity<UserDto> login(@RequestBody LogInIAMRequestDto requestDto);
 
 }
