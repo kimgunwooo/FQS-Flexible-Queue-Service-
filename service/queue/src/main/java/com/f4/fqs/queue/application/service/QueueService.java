efa12c88f3ac77d09a23cb257d4672e97fe93271
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-08 11:03:21 +0900

    chore : 대기열 생성 로직 고도화. (valid 및 추가 정보 처리)

--- service/queue/src/main/java/com/f4/fqs/queue/application/service/QueueService.java
+++ service/queue/src/main/java/com/f4/fqs/queue/application/service/QueueService.java
@@ -1,5 +1,6 @@
 package com.f4.fqs.queue.application.service;
 
+import com.f4.fqs.commons.domain.exception.BusinessException;
 import com.f4.fqs.queue.application.response.CreateQueueResponse;
 import com.f4.fqs.queue.domain.model.Queue;
 import com.f4.fqs.queue.infrastructure.repository.QueueRepository;
@@ -13,28 +14,39 @@ import reactor.core.publisher.Mono;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+import static com.f4.fqs.queue.presentation.exception.QueueErrorCode.QUEUE_NAME_DUPLICATE;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class QueueService {
 
     private final QueueRepository queueRepository;
+    private final QueuePackageService packageService;
 
     @Transactional
-    public Mono<CreateQueueResponse> createQueue(CreateQueueRequest request) {
-        String secretKey = generateRandomString(16);
-        Queue queue = Queue.from(request, secretKey);
+    public Mono<CreateQueueResponse> createQueue(CreateQueueRequest request, Long userId) {
+        String secretKey = generateRandomString();
 
-        return queueRepository.save(queue)
-                .map(q -> new CreateQueueResponse(q.getSecretKey()))
-                .doOnError(e -> log.error("큐 저장 중 오류 발생: {}", e.getMessage()));
+        return queueRepository.existsByName(request.name())
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.error(new BusinessException(QUEUE_NAME_DUPLICATE));
+                    }
+
+                    return packageService.validateQueuePackage(request.queuePackageId())
+                            .flatMap(queuePackage -> {
+                                Queue queue = Queue.from(request, secretKey, userId, queuePackage.getId());
+                                return queueRepository.save(queue)
+                                        .map(q -> new CreateQueueResponse(q.getSecretKey()));
+                            });
+                });
     }
 
-
-    private String generateRandomString(int length) {
+    private String generateRandomString() {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] randomBytes = new byte[length];
+        byte[] randomBytes = new byte[16];
         secureRandom.nextBytes(randomBytes);
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes).substring(0, length);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes).substring(0, 16);
     }
 }
