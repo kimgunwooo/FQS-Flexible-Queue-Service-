d3bf1007de5038e142478df521a677d8904f9fa2
dragon0622 <iamdragon.dev@gmail.com>
2024-10-09 16:16:52 +0900

    feat: dto 추가

---
+++ service/user/src/main/java/com/f4/fqs/user/dto/IAM/LogInIAMRequestDto.java
@@ -0,0 +1,15 @@
+package com.f4.fqs.user.dto.IAM;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class LogInIAMRequestDto {
+
+    private String groupEmail;
+    private String email;
+    private String password;
+}
