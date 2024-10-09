56e186ccfd41f179b38ec521ed17a3a850d31414
dragon0622 <iamdragon.dev@gmail.com>
2024-10-09 16:14:29 +0900

    feat: jwt 관련 수정

--- service/auth/src/main/java/com/f4/fqs/auth/jwt/JwtUtil.java
+++ service/auth/src/main/java/com/f4/fqs/auth/jwt/JwtUtil.java
@@ -1,7 +1,8 @@
 package com.f4.fqs.auth.jwt;
 
-import static javax.crypto.Cipher.SECRET_KEY;
-
+import com.f4.fqs.auth.UserRoleEnum;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -15,6 +16,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtUtil {
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private static final String BEARER_PREFIX = "Bearer ";
     @Value("${spring.application.name}")
     private String issuer;
 
@@ -27,25 +31,54 @@ public class JwtUtil {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
     }
 
-
-    public String createAccessToken(String email) {
-        return Jwts.builder()
-                .claim("email", email)
-                .issuer(issuer)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + accessExpiration))
-                .signWith(secretKey, SignatureAlgorithm.HS512)
-                .compact();
+    public String createAccessToken(Long id, String email, UserRoleEnum role) {
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .claim("id", id)
+                        .claim("email", email)
+                        .claim("role", role)
+                        .issuer(issuer)
+                        .issuedAt(new Date(System.currentTimeMillis()))
+                        .expiration(new Date(System.currentTimeMillis() + accessExpiration))
+                        .signWith(secretKey, SignatureAlgorithm.HS512)
+                        .compact();
     }
 
     public String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (bearerToken != null && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
         }
         return null;
     }
 
-//    public boolean validateToken(String token) {
-//    }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public JwtAuthentication getAuthentication(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Long userId = claims.get("id", Long.class);
+        String email = claims.get("email", String.class);
+        String role = claims.get("role", String.class);
+
+        return new JwtAuthentication(userId, email, role);
+    }
+
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
+    }
 }
