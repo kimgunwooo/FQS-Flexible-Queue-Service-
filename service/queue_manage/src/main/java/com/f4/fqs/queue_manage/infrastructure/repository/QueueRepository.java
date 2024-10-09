a1d23e50efe6c2e6f1f215d9e0864b1f3923ef0a
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-09 18:13:52 +0900

    add : jpa, redis setting

---
+++ service/queue_manage/src/main/java/com/f4/fqs/queue_manage/infrastructure/repository/QueueRepository.java
@@ -0,0 +1,10 @@
+package com.f4.fqs.queue_manage.infrastructure.repository;
+
+import com.f4.fqs.queue_manage.domain.model.Queue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QueueRepository extends JpaRepository<Queue, Long> {
+    boolean existsByName(String name);
+}
