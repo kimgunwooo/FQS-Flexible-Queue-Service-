974ae0760bf177df7a4de72d6e7e9e3e5dd4c089
dragon0622 <iamdragon.dev@gmail.com>
2024-10-09 16:16:41 +0900

    feat: IAM 관련 변경

--- service/user/src/main/java/com/f4/fqs/user/model/IAMUser.java
+++ service/user/src/main/java/com/f4/fqs/user/model/IAMUser.java
@@ -1,12 +1,12 @@
 package com.f4.fqs.user.model;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,14 +14,23 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "p_user")
+@Table(name = "p_single")
 @NoArgsConstructor
-public class User {
+public class IAMUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id",nullable = false)
     private Long id;
+
+    @Column(name = "group_id")
+    private Long groupId;
+
+    @Column(name = "group_name")
+    private String groupName;
+
+    @Column(name = "name")
+    private String name;
 
     @Column(name = "email")
     private String email;
@@ -30,10 +39,14 @@ public class User {
     private String password;
 
     @Column(name = "role")
-    private String role;
+    private String role = "IAM";
 
-    public User(String email, String password, String role) {
+    @Builder
+    public IAMUser(Long groupId, String email, String password, String role, String name, String groupName) {
+        this.groupId = groupId;
         this.email = email;
+        this.name = name;
+        this.groupName = groupName;
         this.password = password;
         this.role = role;
     }
--- service/user/src/main/java/com/f4/fqs/user/repository/IAMRepository.java
+++ service/user/src/main/java/com/f4/fqs/user/repository/IAMRepository.java
@@ -3,7 +3,9 @@ package com.f4.fqs.user.repository;
 import com.f4.fqs.user.model.IAMUser;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAMRepository extends JpaRepository<IAMUser, Long> {
 
     Optional<IAMUser> findByEmail(String email);
