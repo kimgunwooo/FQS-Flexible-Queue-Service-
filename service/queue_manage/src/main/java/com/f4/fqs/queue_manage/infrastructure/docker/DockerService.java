6d5846757f1d9b3a83b27f5a2ff6c3cdcd05cce1
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-14 22:34:54 +0900

    chore : queue server 동적 생성 시 필요한 환경변수 설정

--- service/queue_manage/src/main/java/com/f4/fqs/queue_manage/infrastructure/docker/DockerService.java
+++ service/queue_manage/src/main/java/com/f4/fqs/queue_manage/infrastructure/docker/DockerService.java
@@ -28,9 +28,13 @@ public class DockerService {
         );
 
         String springCommand = String.format(
-                "echo 1541 | sudo -S docker run -d --name %s_queue_server -p %d:8080 -e SPRING_REDIS_HOST=%s_redis -e SPRING_REDIS_PORT=6379 %s",
-                name, springPort, name, springImage
+                "echo 1541 | sudo -S docker run -d --name %s_queue_server -p %d:8080 " +
+                        "-e SERVICE_NAME=%s " +
+                        "-e REDIS_HOST=%s_redis " +
+                        "%s",
+                name, springPort, name, name, springImage
         );
+
 
         executeCommand(redisCommand);
         executeCommand(springCommand);
